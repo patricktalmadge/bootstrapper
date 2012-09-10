3cef42e99649dd3d9729c800af5e030c0eda90a2
Anahkiasen <ehtnam6@gmail.com>
2012-09-11 01:42:04 +0200

    Add tests for Alert and Icons

---
+++ tests/Icons.test.php
@@ -0,0 +1,58 @@
+<?php
+Bundle::start('bootstrapper');
+use Bootstrapper\Icons;
+
+class IconsTest extends PHPUnit_Framework_TestCase
+{
+  private $baseIcon = array(
+    'tag' => 'i',
+    'attributes' => array(
+      'class' => 'icon-folder-open'));
+
+  public function testMake()
+  {
+    $icon = Icons::make('folder-open');
+    $matcher = $this->baseIcon;
+
+    $this->assertTag($matcher, $icon);
+  }
+
+  public function testMakeWithAttributes()
+  {
+    $icon = Icons::make(
+      'folder-open',
+       array('data-foo' => 'bar'));
+    $matcher = $this->baseIcon;
+    $matcher['attributes']['data-foo'] = 'bar';
+    dd($icon);
+
+    $this->assertTag($matcher, $icon);
+  }
+
+  public function testStatic()
+  {
+    $icon = Icons::folder_open();
+    $matcher = $this->baseIcon;
+
+    $this->assertTag($matcher, $icon);
+  }
+
+  public function testStaticWithAttributes()
+  {
+    $icon = Icons::folder_open(array('data-foo' => 'bar'));
+    $matcher = $this->baseIcon;
+    $matcher['attributes']['data-foo'] = 'bar';
+
+    $this->assertTag($matcher, $icon);
+  }
+
+  public function testStaticWhiteWithAttributes()
+  {
+    $icon = Icons::white_folder_open(array('data-foo' => 'bar'));
+    $matcher = $this->baseIcon;
+    $matcher['attributes']['class'] .= ' icon-white';
+    $matcher['attributes']['data-foo'] = 'bar';
+
+    $this->assertTag($matcher, $icon);
+  }
+}
