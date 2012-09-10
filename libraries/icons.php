41e4a7fe7aa983585af231a751c0886e3099fe81
Anahkiasen <ehtnam6@gmail.com>
2012-09-11 01:39:24 +0200

    Fixed a bug when setting attributes to Icons

--- libraries/icons.php
+++ libraries/icons.php
@@ -15,14 +15,14 @@ class Icons
 {
     /**
      * Allows magic methods such as Icons::home([attributes]) or Icons::close_white()
-     * 
+     *
      * Sample Usage:
      * <code>
      * <?php
      * Icons::plus();
      * // <i class="icon-plus"></i>
      * Icons::folder_open(array('class'=>'widget','data-foo'=>'bar'));
-     * // <i class="widget icon-folder-open" data-foo="bar"></i> 
+     * // <i class="widget icon-folder-open" data-foo="bar"></i>
      * Icons::circle_arrow_right_white();
      * // <i class="icon-circle-arrow-right icon-white"></i>
      * ?>
@@ -36,29 +36,33 @@ class Icons
 	{
 		// Explode method name
 		$method_bits = explode('_', strtolower($method));
-		
-		//white icon variant? (when using glyphicons sprite version)
-		$white = in_array('white',$method_bits);
-		
-		//remove the white!
-		$method_bits = array_filter($method_bits,function($val){ return ($val != 'white'); });
-		
+
+		// White icon variant? (when using glyphicons sprite version)
+		$white = in_array('white', $method_bits);
+
+		// Remove white from array
+		$method_bits = array_filter($method_bits, function($val) {
+            return ($val != 'white');
+        });
+
 		// Get icon name
 		$icon_classes = array(implode('-',$method_bits));
 		if($white) $icon_classes[] = 'white';
-		
+
 		// Prepend icon- to classes
 		$parameters = Helpers::set_multi_class_attributes(null, $icon_classes, $attributes, 0, 'icon-');
+        $parameters = array_merge($parameters, $parameters[0]);
+        unset($parameters[0]);
 
-		return '<i'.HTML::attributes($parameters[0]).'></i>';
+		return '<i'.HTML::attributes($parameters).'></i>';
 	}
 
     /**
      * Return icon HTML using alternate syntax.
      * Overload via __callStatic() allows calls like Icons::check() or Icons::paper_clip_white()
      * but code-inspecting IDEs will show the method as undefined, and there are just way too many
-     * icon classes to use @ method docblock instead  
-     * 
+     * icon classes to use @ method docblock instead
+     *
      * Sample Usage:
      * <code>
      * <?php
@@ -66,7 +70,7 @@ class Icons
      * // <i class="widget icon-folder-open"></i>
      * ?>
      * </code>
-     * 
+     *
      * @static
      * @param $icon_class
      * @param null $attributes
