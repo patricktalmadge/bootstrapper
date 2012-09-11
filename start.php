fc72b4b471eddc629494af15d380fba80c59b7fd
Maxime Fabre <ehtnam6@gmail.com>
2012-09-11 11:58:13 +0200

    Group calls to Asset for easier readability and better performances

--- start.php
+++ start.php
@@ -18,19 +18,12 @@ Autoloader::namespaces(
   array('Bootstrapper' => Bundle::path('bootstrapper') . 'libraries')
 );
 
-Asset::container('bootstrapper')->bundle('bootstrapper');
+// Define main assets
+Asset::container('bootstrapper')
+  ->bundle('bootstrapper')
+    ->add('bootstrap',            'css/bootstrap.min.css')
+    ->add('bootstrap-responsive', 'css/bootstrap-responsive.min.css')
+    ->add('nav-fix',              'css/nav-fix.css')
+    ->add('jquery',               'js/jquery-1.8.1.min.js')
+    ->add('bootstrap-js',         'js/bootstrap.min.js');
 
-// Asset::container('bootstrapper')->add('jquery', 'js/jquery-1.8.1.js');
-Asset::container('bootstrapper')->add('jquery', 'js/jquery-1.8.1.min.js');
-
-// Asset::container('bootstrapper')->add('bootstrap', 'css/bootstrap.css');
-Asset::container('bootstrapper')->add('bootstrap', 'css/bootstrap.min.css');
-
-// Not Needed if you don't have the top nav bar
-Asset::container('bootstrapper')->add('nav-fix', 'css/nav-fix.css');
-
-// Asset::container('bootstrapper')->add('bootstrap-responsive', 'css/bootstrap-responsive.css');
-Asset::container('bootstrapper')->add('bootstrap-responsive', 'css/bootstrap-responsive.min.css');
-
-// Asset::container('bootstrapper')->add('bootstrap-js', 'js/bootstrap.js');
-Asset::container('bootstrapper')->add('bootstrap-js', 'js/bootstrap.min.js');
