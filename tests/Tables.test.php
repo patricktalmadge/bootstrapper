917f52b3ff3e0573eedd22e11cc60269a9ee0eea
Maxime Fabre <ehtnam6@gmail.com>
2012-09-12 10:38:31 +0200

    Add tests for Tables::table, Tables::headers and Tables::close

--- tests/Tables.test.php
+++ tests/Tables.test.php
@@ -3,11 +3,66 @@ use Bootstrapper\Tables;
 
 class TablesTest extends BootstrapperWrapper
 {
+  private $matcher = array(
+    'tag' => 'table',
+    'attributes' => array(
+      'data-foo' => 'bar',
+      'class' => 'foo table'
+    ),
+  );
+
   public function testOpen()
   {
-    $table = Tables::open();
-    $matcher = array('tag' => 'table', 'attributes' => array('class' => 'table'));
+    $table = Tables::open($this->testAttributes);
+
+    $this->assertTag($this->matcher, $table);
+  }
+
+  public function testStaticOpen()
+  {
+    $table = Tables::bordered_condensed_foobar($this->testAttributes);
+    $matcher = $this->matcher;
+    $matcher['attributes']['class'] = 'foo table-bordered table-condensed table';
 
     $this->assertTag($matcher, $table);
+  }
+
+  public function testClose()
+  {
+    $close = Tables::close();
+
+    $this->assertEquals('</table>', $close);
+  }
+
+  public function testHeadersSimple()
+  {
+    $headers = Tables::headers(array('foo', 'bar', 'tel', 'sub'));
+    $headers = str_replace(PHP_EOL, null, $headers);
+
+    $matcher =
+    '<thead>'.
+      '<th>foo</th>'.
+      '<th>bar</th>'.
+      '<th>tel</th>'.
+      '<th>sub</th>'.
+    '</thead>';
+
+    $this->assertEquals($matcher, $headers);
+  }
+
+  public function testHeadersComplex()
+  {
+    $headers = Tables::headers(array(
+      'foo' => $this->testAttributes,
+      'bar' => $this->testAttributes));
+    $headers = str_replace(PHP_EOL, null, $headers);
+
+    $matcher =
+    '<thead>'.
+      '<th class="foo" data-foo="bar">foo</th>'.
+      '<th class="foo" data-foo="bar">bar</th>'.
+    '</thead>';
+
+    $this->assertEquals($matcher, $headers);
   }
 }
