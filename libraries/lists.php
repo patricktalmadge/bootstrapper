3abc5ddedad9f185687d94c1c65ee2f4171bd8fd
Maxime Fabre <ehtnam6@gmail.com>
2012-09-14 15:34:28 +0200

    Add Lists class to format lists and definition lists

---
+++ libraries/lists.php
@@ -0,0 +1,55 @@
+<?php
+namespace bootstrapper;
+
+use \HTML;
+
+/**
+ * Class for creating lists and definition lists
+ *
+ * @package     Bundles
+ * @subpackage  Twitter
+ * @author      Patrick Talmadge - Follow @patricktalmadge
+ *
+ * @see http://twitter.github.com/bootstrap/
+ */
+class Lists
+{
+  /**
+   * Creates a definition list
+   *
+   * @param  array  $list       An array [term => description]
+   * @param  array  $attributes An array of attributes
+   * @return string             A formatted <dl> list
+   */
+  public static function dl($list, $attributes = array())
+  {
+    $dl = null;
+
+    // Check if the list contains anything
+    if (count($list) == 0) {
+      return false;
+    }
+
+    // Write each entry
+    foreach ($list as $term => $description) {
+      $dl .= '<dt>'.HTML::entities($term).'</dt>';
+      $dl .= '<dd>'.HTML::entities($description).'</dd>';
+    }
+
+    return '<dl'.HTML::attributes($attributes).'>'.$dl.'</dl>';
+  }
+
+  /**
+   * Creates an horizontal definition list
+   *
+   * @param  array  $list       An array [term => description]
+   * @param  array  $attributes An array of attributes
+   * @return string             A formatted <dl> list
+   */
+  public static function horizontal_dl($list, $attributes = array())
+  {
+    $attributes = Helpers::add_class($attributes, 'dl-horizontal');
+
+    return static::dl($list, $attributes);
+  }
+}
