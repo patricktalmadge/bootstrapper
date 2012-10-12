4e9da2968b1a49f409f3287055204648eb3e7ddc
Anahkiasen <ehtnam6@gmail.com>
2012-10-12 12:06:30 +0200

    Refactor Carousel class

--- libraries/carousel.php
+++ libraries/carousel.php
@@ -19,16 +19,59 @@ use \HTML;
 class Carousel
 {
     /**
+     * The current Carousel instance
+     * @var Carousel
+     */
+    private static $carousel;
+
+    /**
+     * How data will be fetched from each object/entry
+     * @var array
+     */
+    private static $schema = array(
+        'key'        => 'id',
+        'alt'        => 'alt_text',
+        'attributes' => 'attributes',
+        'caption'    => 'caption',
+        'label'      => 'label',
+        'image'      => 'image',
+    );
+
+    /**
+     * The carousel's items
+     * @var array
+     */
+    private $items = array();
+
+    /**
+     * The carousel's attributes
+     * @var array
+     */
+    private $attributes = array();
+
+    /**
      * The previous button content
      * @var string
      */
-    public static $prev = '&lsaquo;';
+    private $prev = '&lsaquo;';
 
     /**
      * The next button content
      * @var string
      */
-    public static $next = '&rsaquo;';
+    private $next = '&rsaquo;';
+
+    /**
+     * The current carousel's hash
+     * @var string
+     */
+    private $hash = null;
+
+    /**
+     * The current active element in the carousel
+     * @var integer
+     */
+    private $active = null;
 
     /**
      * Create a Bootstrap carousel. Returns the HTML for the carousel.
@@ -40,51 +83,130 @@ class Carousel
      */
     public static function create($items, $attributes = array())
     {
-        $attributes = Helpers::add_class($attributes, 'carousel slide');
+        static::$carousel = new static($items, $attributes);
+
+        return static::$carousel;
+    }
+
+    /**
+     * Creates a new Carousel instance
+     *
+     * @param array  $items      The items to use as pictures
+     * @param array  $attributes Its attributes
+     */
+    public function __construct($items, $attributes = array())
+    {
+        $this->items = $items;
+        $this->attributes = Helpers::add_class($attributes, 'carousel slide');
+
+        // Set default active item
+        $this->active = key($items);
 
         // Calculate the Carousel ID
-        $carousel_id = '#'.array_get($attributes, 'id', 'carousel_'.Helpers::rand_string(5));
+        $this->hash = '#'.array_get($attributes, 'id', 'carousel_'.Helpers::rand_string(5));
+    }
 
-        // Render main wrapper
-        $html = '<div'.HTML::attributes($attributes).'>';
-
-        // Render items
-        $html .= '<div class="carousel-inner">';
-        foreach ($items as $key => $item) {
-            $html .= static::createItem($item, $key == key($items));
+    /**
+     * Magic methods for the Carousel class
+     *
+     * @param  string   $method     The method
+     * @param  array    $parameters Its parameters
+     * @return Carousel
+     */
+    public function __call($method, $parameters)
+    {
+        // Dynamic schema editing
+        // Example : ->as_label('name') and $item->name/$item['name'] will be used as label
+        if(starts_with($method, 'as_')) {
+            $use = substr($method, 3);
+            if(!isset(static::$schema[$use])) return $this;
+            else {
+                static::$schema[$use] = array_get($parameters, 0);
+            }
         }
-        $html .= '</div>';
 
-        // Render navigation
-        $html .= HTML::link($carousel_id, Carousel::$prev, array('class' => 'carousel-control left', 'data-slide' => 'prev'));
-        $html .= HTML::link($carousel_id, Carousel::$next, array('class' => 'carousel-control right', 'data-slide' => 'next'));
+        return $this;
+    }
+
+    /**
+     * Set which element will be the active one
+     *
+     * @param  integer $key A key
+     * @return Carousel
+     */
+    public function active($key)
+    {
+        $this->active = $key;
+
+        return $this;
+    }
+
+    /**
+     * Set the current Carousel's #id
+     *
+     * @param  string $id The new id
+     * @return Carousel
+     */
+    public function id($id)
+    {
+        $this->hash = '#'.$id;
+
+        return $this;
+    }
+
+    /**
+     * Prints out the current Carousel instance
+     *
+     * @return string A carousel
+     */
+    public function __toString()
+    {
+        // Render main wrapper
+        $this->attributes['id'] = substr($this->hash, 1);
+        $html = '<div'.HTML::attributes($this->attributes).'>';
+
+            // Render items
+            $html .= '<div class="carousel-inner">';
+                foreach ($this->items as $key => $item) {
+                    $html .= $this->createItem($item, $key);
+                }
+            $html .= '</div>';
+
+            // Render navigation
+            $html .= HTML::link($this->hash, $this->prev, array('class' => 'carousel-control left',  'data-slide' => 'prev'));
+            $html .= HTML::link($this->hash, $this->next, array('class' => 'carousel-control right', 'data-slide' => 'next'));
         $html .= '</div>';
 
         return $html;
     }
 
+    //////////////////////////////////////////////////////////////////
+    ///////////////////////////// HELPERS ////////////////////////////
+    //////////////////////////////////////////////////////////////////
+
     /**
      * Create a carousel item. Returns a HTML element for one slide.
      *
-     * @param array $item      A carousel item to render
-     * @param bool  $is_active Whether the item is active or not
+     * @param array $item  A carousel item to render
+     * @param bool  $key   A fallback key as the item's position in the array
      *
      * @return string
      */
-    protected static function createItem($item, $is_active)
+    protected function createItem($item, $key)
     {
         // Gather necessary variables
-        $active     = $is_active ? ' active' : '';
-        $altText    = array_get($item, 'alt_text', '');
-        $attributes = array_get($item, 'attributes', array());
-        $caption    = array_get($item, 'caption', '');
-        $label      = array_get($item, 'label', '');
-        $image      = array_get($item, 'image');
+        $key        = static::getFromItem($item, 'key', $key);
+        $altText    = static::getFromItem($item, 'alt');
+        $attributes = static::getFromItem($item, 'attributes', array());
+        $caption    = static::getFromItem($item, 'caption');
+        $label      = static::getFromItem($item, 'label');
+        $image      = static::getFromItem($item, 'image');
 
         // If we were given an array of image paths instead of arrays
         if (!$image and is_string($item)) $image = $item;
 
         // Build HTML
+        $active = $this->active == $key ? ' active' : null;
         $html = '<div class="item'.$active.'">';
 
         // Render the image
@@ -93,7 +215,7 @@ class Carousel
         // If we have a caption, render it
         if ($caption or $label) {
             $html .= '<div class="carousel-caption">';
-            if ($label) $html .= '<h4>'.$label.'</h4>';
+            if ($label)   $html .= '<h4>'.$label.'</h4>';
             if ($caption) $html .= '<p>'.$caption.'</p>';
             $html .= '</div>';
         }
@@ -101,5 +223,24 @@ class Carousel
         $html .= '</div>';
 
         return $html;
+    }
+
+    /**
+     * Get a piece of data from an item
+     *
+     * @param  mixed  $item     The item
+     * @param  string $key      The key to fetch
+     * @param  string $fallback A fallback to use
+     * @return string           A data from the item
+     */
+    private static function getFromItem($item, $key, $fallback = null)
+    {
+        $key = static::$schema[$key];
+
+        if(is_object($item)) {
+            return isset($item->$key) ? $item->$key : $fallback;
+        } else {
+            return array_get($item, $key, $fallback);
+        }
     }
 }
