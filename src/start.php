a58ee5cda6fee6c52ec770365b3368e057e6a480
Patrick Talmadge <ptalmadge@gmail.com>
2012-11-09 18:46:47 -0800

    Comment out Auto alias add for but submodule bundles

--- src/start.php
+++ src/start.php
@@ -37,31 +37,31 @@ Asset::container('bootstrapper-unminified')
     ->add('bootstrap-js',         'js/bootstrap.js', 'jquery');
 
 // Prevent the need for modifying config/application.php
-$custom_aliases = array(
-    'Alert'          => 'Bootstrapper\\Alert',
-    'Badge'          => 'Bootstrapper\\Badge',
-    'Breadcrumb'     => 'Bootstrapper\\Breadcrumb',
-    'Button'         => 'Bootstrapper\\Button',
-    'ButtonGroup'    => 'Bootstrapper\\ButtonGroup',
-    'ButtonToolbar'  => 'Bootstrapper\\ButtonToolbar',
-    'Carousel'       => 'Bootstrapper\\Carousel',
-    'DropdownButton' => 'Bootstrapper\\DropdownButton',
-    'Form'           => 'Bootstrapper\\Form',
-    'Helpers'        => 'Bootstrapper\\Helpers',
-    'Icon'           => 'Bootstrapper\\Icon',
-    'Image'          => 'Bootstrapper\\Image',
-    'Label'          => 'Bootstrapper\\Label',
-    'MediaObject'    => 'Bootstrapper\\MediaObject',
-    'Navbar'         => 'Bootstrapper\\Navbar',
-    'Navigation'     => 'Bootstrapper\\Navigation',
-    'Paginator'      => 'Bootstrapper\\Paginator',
-    'Progress'       => 'Bootstrapper\\Progress',
-    'Tabbable'       => 'Bootstrapper\\Tabbable',
-    'Table'          => 'Bootstrapper\\Table',
-    'Thumbnail'      => 'Bootstrapper\\Thumbnail',
-    'Typeahead'      => 'Bootstrapper\\Typeahead',
-    'Typography'     => 'Bootstrapper\\Typography',
-);
+// $custom_aliases = array(
+//     'Alert'          => 'Bootstrapper\\Alert',
+//     'Badge'          => 'Bootstrapper\\Badge',
+//     'Breadcrumb'     => 'Bootstrapper\\Breadcrumb',
+//     'Button'         => 'Bootstrapper\\Button',
+//     'ButtonGroup'    => 'Bootstrapper\\ButtonGroup',
+//     'ButtonToolbar'  => 'Bootstrapper\\ButtonToolbar',
+//     'Carousel'       => 'Bootstrapper\\Carousel',
+//     'DropdownButton' => 'Bootstrapper\\DropdownButton',
+//     'Form'           => 'Bootstrapper\\Form',
+//     'Helpers'        => 'Bootstrapper\\Helpers',
+//     'Icon'           => 'Bootstrapper\\Icon',
+//     'Image'          => 'Bootstrapper\\Image',
+//     'Label'          => 'Bootstrapper\\Label',
+//     'MediaObject'    => 'Bootstrapper\\MediaObject',
+//     'Navbar'         => 'Bootstrapper\\Navbar',
+//     'Navigation'     => 'Bootstrapper\\Navigation',
+//     'Paginator'      => 'Bootstrapper\\Paginator',
+//     'Progress'       => 'Bootstrapper\\Progress',
+//     'Tabbable'       => 'Bootstrapper\\Tabbable',
+//     'Table'          => 'Bootstrapper\\Table',
+//     'Thumbnail'      => 'Bootstrapper\\Thumbnail',
+//     'Typeahead'      => 'Bootstrapper\\Typeahead',
+//     'Typography'     => 'Bootstrapper\\Typography',
+// );
 
-$laravel_aliases = Laravel\Autoloader::$aliases;
-Laravel\Autoloader::$aliases = array_merge($laravel_aliases, $custom_aliases);
+// $laravel_aliases = Laravel\Autoloader::$aliases;
+// Laravel\Autoloader::$aliases = array_merge($laravel_aliases, $custom_aliases);
