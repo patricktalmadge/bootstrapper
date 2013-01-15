ce1b03dc3424584633e6dd7197a5660a875ab020
Anahkiasen <ehtnam6@gmail.com>
2013-01-15 01:28:21 +0100

    Remove stuff

--- tests/_start.php
+++ tests/_start.php
@@ -5,12 +5,4 @@ abstract class BootstrapperWrapper extends PHPUnit_Framework_TestCase
     'class'    => 'foo',
     'data-foo' => 'bar',
   );
-
-  public static function startUpBeforeClass()
-  {
-    $url = Mockery\Mockery::mock('UrlGenerator');
-
-    $app = Mockery\Mockery::mock('alias:App');
-    $app->shouldReceive('make')->with('url')->andReturn($url);
-  }
 }
