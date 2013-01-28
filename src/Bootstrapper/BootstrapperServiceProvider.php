0a163fbafaf2391a820cfcdf9cf0b2ae4710a7b5
Anahkiasen <ehtnam6@gmail.com>
2013-01-28 01:25:30 +0100

    Register Basset container in the Service Provider — remove old start file

--- src/Bootstrapper/BootstrapperServiceProvider.php
+++ src/Bootstrapper/BootstrapperServiceProvider.php
@@ -12,6 +12,24 @@ class BootstrapperServiceProvider extends ServiceProvider
    */
   public function register()
   {
+    // Manually register Basset as we need it now
+    include __DIR__.'/../../../../jasonlewis/basset/src/Basset/BassetServiceProvider.php';
+
     $this->package('patricktalmadge/bootstrapper');
+  }
+
+  /**
+   * Register assets
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->app['basset']->collection('bootstrapper', function($collection) {
+      $collection->add('packages/patricktalmadge/bootstrapper/css/bootstrap.min.css');
+      $collection->add('packages/patricktalmadge/bootstrapper/css/bootstrap-responsive.min.css');
+
+      $collection->add('packages/patricktalmadge/bootstrapper/js/jquery-1.8.3.min.css');
+    });
   }
 }
