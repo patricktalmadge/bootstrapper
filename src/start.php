95d2204b682d8e516bb8bf49be3aa2f938a07efe
Anahkiasen <ehtnam6@gmail.com>
2013-02-15 12:02:03 +0100

    Update Bootstrap and jQuery to latest

--- src/start.php
+++ src/start.php
@@ -23,7 +23,7 @@ Asset::container('bootstrapper')
     ->bundle('bootstrapper')
     ->add('bootstrap',            'css/bootstrap.min.css')
     ->add('bootstrap-responsive', 'css/bootstrap-responsive.min.css')
-    ->add('jquery',               'js/jquery-1.8.3.min.js')
+    ->add('jquery',               'js/jquery-1.9.1.min.js')
     ->add('bootstrap-js',         'js/bootstrap.min.js', 'jquery');
 
 // Define unminified version of the assets
@@ -31,5 +31,5 @@ Asset::container('bootstrapper-unminified')
     ->bundle('bootstrapper')
     ->add('bootstrap',            'css/bootstrap.css')
     ->add('bootstrap-responsive', 'css/bootstrap-responsive.css')
-    ->add('jquery',               'js/jquery-1.8.3.js')
+    ->add('jquery',               'js/jquery-1.9.1.js')
     ->add('bootstrap-js',         'js/bootstrap.js', 'jquery');
