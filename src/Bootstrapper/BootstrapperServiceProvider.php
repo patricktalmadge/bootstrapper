f88fe3faf46eb02e0416b06014931710721dd5aa
Dries Vints <dries.vints@gmail.com>
2013-03-19 21:04:16 +0100

    Replaced spaces with tabs in ServiceProvider file
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- src/Bootstrapper/BootstrapperServiceProvider.php
+++ src/Bootstrapper/BootstrapperServiceProvider.php
@@ -5,41 +5,42 @@ use LaravelBook\Laravel4Powerpack\Form as MeidoForm;
 
 // Manually register Basset as we need it now
 if (!class_exists('Basset\BassetServiceProvider')) {
-  $basset = __DIR__.'/../../../../jasonlewis/basset/src/Basset/BassetServiceProvider.php';
-  if (file_exists($basset)) include $basset;
+	$basset = __DIR__.'/../../../../jasonlewis/basset/src/Basset/BassetServiceProvider.php';
+	if (file_exists($basset)) include $basset;
 }
 
-class BootstrapperServiceProvider extends ServiceProvider
-{
-  /**
-   * Register the service provider.
-   *
-   * @return void
-   */
-  public function register()
-  {
-    $this->package('patricktalmadge/bootstrapper');
+class BootstrapperServiceProvider extends ServiceProvider {
 
-    $this->app['config']->package('patricktalmadge/bootstrapper', __DIR__. '/../config');
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->package('patricktalmadge/bootstrapper');
 
-    $app = $this->app;
-    $this->app->singleton('form', function($app) {
-      return new MeidoForm($app['url']);
-    });
-  }
+		$this->app['config']->package('patricktalmadge/bootstrapper', __DIR__. '/../config');
 
-  /**
-   * Register assets
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    if (!is_dir($this->app['path.public'].'/packages/patricktalmadge/')) return false;
+		$app = $this->app;
+		$this->app->singleton('form', function($app) {
+			return new MeidoForm($app['url']);
+		});
+	}
 
-    $this->app['config']->set('basset::collections.bootstrapper', function($collection) {
-      $collection->requireDirectory('packages/patricktalmadge/bootstrapper/css');
-      $collection->requireDirectory('packages/patricktalmadge/bootstrapper/js');
-    });
-  }
+	/**
+	 * Register assets
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		if (!is_dir($this->app['path.public'].'/packages/patricktalmadge/')) return false;
+
+		$this->app['config']->set('basset::collections.bootstrapper', function($collection) {
+			$collection->requireDirectory('packages/patricktalmadge/bootstrapper/css');
+			$collection->requireDirectory('packages/patricktalmadge/bootstrapper/js');
+		});
+	}
+
 }
