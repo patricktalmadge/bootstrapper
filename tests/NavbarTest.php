61a5a798a494250d4423171b1efd1f629cf09573
Marvin Schröder <schroeder@mediameets.de>
2013-09-13 11:26:48 +0200

    Update NavbarTest.php

--- tests/NavbarTest.php
+++ tests/NavbarTest.php
@@ -8,7 +8,7 @@ class NavbarTest extends BootstrapperWrapper
   {
     $matcher = array(
       'tag' => 'nav',
-      'attributes' => array('class' => 'navbar'),
+      'attributes' => array('class' => 'navbar '),
       'child' => array(
         'tag' => 'div',
         'attributes' => array('class' => 'navbar-header')
@@ -20,25 +20,23 @@ class NavbarTest extends BootstrapperWrapper
       $matcher['child']['child'] = array(
         'tag' => 'button',
         'attributes' => array(
-          'class' => 'navbar-toggle',
+          'class' => 'btn btn-navbar navbar-toggle',
           'data-toggle' => 'collapse',
           'data-target' => '.navbar-collapse',
           'type' => 'button'          
-        ),
-        'descendant' => array(
-            'tag' => 'span',
-            'attributes' => array('class' => 'sr-only'),
-            'content' => 'Toggle navigation'
-          ),
+        ),        
         'children' => array(
           'count' => 3,
           'only' => array(
             'tag' => 'span',
             'class' => 'icon-bar'
-          ),
-          
-          
+          ),         
         ),
+      );
+      $matcher['child']['child']['descendant'] = array(
+        'tag' => 'span',
+        'attributes' => array('class' => 'sr-only'),
+        'content' => 'Toggle navigation'
       );
       $matcher['descendant'] = array(
         'tag' => 'div',
