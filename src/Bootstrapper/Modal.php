40d65141fb7523febf23bc75d4d8a60c7f5905b8
Patrick Rose <pjr0911025+github@googlemail.com>
2013-12-26 19:21:13 +0000

    Implement modals for #146

---
+++ src/Bootstrapper/Modal.php
@@ -0,0 +1,101 @@
+<?php namespace Bootstrapper;
+
+
+/**
+ * Class for creating Twitter Bootstrap Modals.
+ *
+ * @category   HTML/UI
+ * @package    Boostrapper
+ * @subpackage Twitter
+ * @author     Patrick Talmadge - <ptalmadge@gmail.com>
+ * @author     Maxime Fabre - <ehtnam6@gmail.com>
+ * @author     Patrick Rose - <pjr0911025@gmail.com>
+ * @license    MIT License <http://www.opensource.org/licenses/mit>
+ * @link       http://laravelbootstrapper.phpfogapp.com/
+ *
+ * @see        http://twitter.github.com/bootstrap/
+ */
+class Modal {
+
+    protected $attributes;
+
+    protected $header;
+
+    protected $body;
+
+    protected $footer;
+
+    public static function create($label, $attributes = null) {
+        $attributes = Helpers::add_class($attributes, $label, "aria-labelledby");
+
+        return new static($attributes);
+    }
+
+    public static function withHeader($headerText, $label, $attributes = null) {
+        $attributes = Helpers::add_class($attributes, $label, "aria-labelledby");
+        $modal = new static($attributes, $headerText);
+        return $modal;
+    }
+
+    public static function withBody($bodyText, $label, $attributes = null) {
+        $attributes = Helpers::add_class($attributes, $label, "aria-labelledby");
+        return new static($attributes, null, $bodyText);
+    }
+
+    public static function withFooter($footerText, $label, $attributes = null) {
+        $attributes = Helpers::add_class($attributes, $label, "aria-labelledby");
+        return new static($attributes, null, null, $footerText);
+    }
+
+    public function __construct($attributes, $header = null, $body = null, $footer = null) {
+        $this->attributes = $attributes;
+        $this->header = $header;
+        $this->body = $body;
+        $this->footer = $footer;
+    }
+
+    public function header($headerText) {
+        $this->header = $headerText;
+        return $this;
+    }
+
+    public function body($bodyText) {
+        $this->body = $bodyText;
+        return $this;
+    }
+
+    public function footer($footerText) {
+	$this->footer = $footerText;
+	return $this;
+    }
+
+    public function render() {
+        $this->attributes = Helpers::add_class($this->attributes, 'modal');
+        $this->attributes = Helpers::add_class($this->attributes, "true", 'aria-hidden');
+        // Open the modal
+        $string = "<div" . Helpers::getContainer('html')->attributes($this->attributes) . ">";
+        // Add the header
+        $string .= '<div class="modal-header">';
+        $string .= '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>';
+        if ($this->header) {
+            $string .= "<h3>" . $this->header . "</h3>";
+        }
+        $string .= "</div>";
+        if ($this->body) {
+            $string .= "<div class='modal-body'>";
+            $string .= $this->body;
+            $string .= "</div>";
+        }
+        if ($this->footer) {
+            $string .= "<div class='modal-footer'>";
+            $string .= $this->footer;
+            $string .= "</div>";
+        }
+        return $string . "</div>";
+    }
+
+    public function __toString() {
+        return $this->render();
+    }
+
+}
---
+++ tests/ModalTest.php
@@ -0,0 +1,164 @@
+<?php
+
+use Bootstrapper\Modal;
+
+class ModalTest extends BootstrapperWrapper
+{
+
+    protected function getMatcher() {
+        $matcher = array(
+            'tag' => 'div',
+            'attributes' => array(
+                'class' => 'foo modal',
+                'aria-labelledby' => "bar",
+                'aria-hidden' => "true"
+            ),
+            'child' => array(
+                'tag' => 'div',
+                'attributes' => array(
+                    'class' => 'modal-header'
+                ),
+                'child' => array(
+                    'tag' => 'button',
+                    'attributes' => array(
+                        'type' => 'button',
+                        'class' => 'close',
+                        'data-dismiss' => 'modal',
+                        'aria-hidden' => 'true'
+                    ),
+                    'content' => 'x'
+                ),
+            )
+        );
+        return $matcher;
+    }
+
+    protected function getAttributes() {
+        return array('class' => 'foo');
+    }
+
+    public function testCanOpenModel() {
+        $modal = Modal::create("bar", $this->getAttributes());
+        $matcher = $this->getMatcher();
+        $this->assertHTML($matcher, $modal);
+    }
+
+    public function testCanCreateWithHeader() {
+        $modal = Modal::withHeader("Foo Bar", "bar", $this->getAttributes());
+        $matcher = $this->getMatcher();
+        $matcher['child']['descendant'] =
+        array(
+            'tag' => "h3",
+            'attributes' => array(
+            ),
+            'content' => 'Foo Bar'
+        );
+        $this->assertHTML($matcher, $modal);
+    }
+
+    public function testCanCreateWithBody() {
+        $modal = Modal::withBody("Foo bar", "bar", $this->getAttributes());
+        $matcher = $this->getMatcher();
+        $matcher['descendant'] = array(
+            'tag' => 'div',
+            'attributes' => array(
+                'class' => 'modal-body'
+            ),
+            'content' => 'Foo bar'
+        );
+        $this->assertHTML($matcher, $modal);
+    }
+
+    public function testCanCreateWithFooter() {
+
+        $modal = Modal::withFooter("Foo bar", "bar", $this->getAttributes());
+
+        $matcher = $this->getMatcher();
+        $matcher['descendant'] = array(
+            'tag' => 'div',
+            'attributes' => array(
+                'class' => 'modal-footer'
+            ),
+            'content' => "Foo bar",
+        );
+
+        $this->assertHTML($matcher, $modal);
+    }
+
+    public function testCanAddAHeader() {
+
+        $modal = Modal::create("bar", $this->getAttributes())->header("Foo bar");
+
+        $matcher = $this->getMatcher();
+        $matcher['child']['descendant'] =
+        array(
+            'tag' => "h3",
+            'content' => 'Foo bar'
+        );
+        $this->assertHTML($matcher, $modal);
+    }
+
+    public function testCanAddBodyText() {
+
+        $modal = Modal::create("bar", $this->getAttributes())->body("Foo bar");
+        $matcher = $this->getMatcher();
+        $matcher['descendant'] = array(
+            'tag' => 'div',
+            'attributes' => array(
+                'class' => 'modal-body'
+            ),
+            'content' => 'Foo bar'
+        );
+        $this->assertHTML($matcher, $modal);
+
+    }
+
+    public function testCanAddFooter() {
+
+        $modal = Modal::create("bar", $this->getAttributes())->footer("Foo bar");
+
+        $matcher = $this->getMatcher();
+        $matcher['descendant'] = array(
+            'tag' => 'div',
+            'attributes' => array(
+                'class' => 'modal-footer'
+            ),
+            'content' => "Foo bar",
+        );
+
+        $this->assertHTML($matcher, $modal);
+    }
+
+    public function testCanCreateAndAddHeaderBodyAndFooter() {
+
+        $modal = Modal::create("bar", $this->getAttributes())->footer("Foo bar")->body("Foo bar")->header("Foo bar");
+
+        $matcher = array(
+            'tag' => 'div',
+            'attributes' => array(
+                'class' => 'modal-body'
+            ),
+            'content' => 'Foo bar'
+        );
+	$parent = $this->getMatcher();
+        $parent['descendant'] = array(
+            'tag' => 'div',
+            'attributes' => array(
+                'class' => 'modal-footer'
+            ),
+            'content' => "Foo bar",
+        );
+        $parent['child']['descendant'] =
+        array(
+            'tag' => "h3",
+            'attributes' => array(
+            ),
+            'content' => 'Foo bar'
+        );
+	$matcher['parent'] = $parent;
+
+	$this->assertHTML($matcher, $modal);
+	
+    }
+
+}
