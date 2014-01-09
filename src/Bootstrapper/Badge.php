f2ed8bddc1f3cd1ecbccc57ee172c91d679e8f1d
Patrick Rose <pjr0911025+github@googlemail.com>
2014-01-09 16:16:55 +0000

    Fix badges

--- src/Bootstrapper/Badge.php
+++ src/Bootstrapper/Badge.php
@@ -19,34 +19,35 @@ use Bootstrapper\Traits\ClassableElement;
  */
 class Badge extends ClassableElement
 {
-    /**
-     * The base class
-     *
-     * @var string
-     */
-    protected static $baseClass = 'badge';
+  /**
+   * The base class
+   *
+   * @var string
+   */
+  protected static $baseClass = 'badge';
 
-    /**
-     * Create a custom label (this is here for backward compatibility)
-     *
-     * @param string $type       The badge type
-     * @param string $message    The content
-     * @param array  $attributes The attributes
-     *
-     * @return Badge
-     */
-    public static function custom($type, $message, $attributes)
-    {
-        return static::$type($message, $attributes);
-    }
+  /**
+   * Create a custom label (this is here for backward compatibility)
+   *
+   * @param string $type       The badge type
+   * @param string $message    The content
+   * @param array  $attributes The attributes
+   *
+   * @return Badge
+   */
+  public static function custom($type, $message, $attributes)
+  {
+    return static::$type($message, $attributes);
+  }
 
-    /**
-     * Bootstrap no longer has specific badge classes, so we seperate
-     * them.
-     */
-    public static function __callStatic($method, $parameters)
-    {
-        $badge = parent::$method($parameters[0], $parameters[1]);
-        return str_replace('badge-', null, str_replace('default', null, $badge));
-    }
+  /**
+   * Bootstrap no longer has specific badge classes, so we seperate
+   * them.
+   */
+  public static function __callStatic($method, $parameters)
+  {
+    $parameters[1] = isset($parameters[1]) ? $parameters[1] : array(); 
+    $badge = parent::$method($parameters[0], $parameters[1]);
+    return str_replace('badge-', null, str_replace('default', null, $badge));
+  }
 }
