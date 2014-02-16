936792ae7469cb04b7ed658e6ac38113a1d40781
Patrick Rose <pjr0911025+github@googlemail.com>
2014-02-16 11:09:03 +0000

    Implement accordions

--- src/Bootstrapper/Accordion.php
+++ src/Bootstrapper/Accordion.php
@@ -6,6 +6,10 @@ class Accordion {
 
   protected $attributes;
 
+  protected $contents = array();
+
+  protected $opened = 0;
+
   public function __construct($name, $attributes) {
     $this->name = $name;
     $this->attributes = $attributes;
@@ -15,12 +19,45 @@ class Accordion {
     return new static($name, $attributes);
   }
 
-  public function __toString() {
-    $attributes = Helpers::add_class($this->attributes, 'panel-group');
-    $attributes = Helpers::add_class($attributes, $this->name, 'id');
-    
-    $string = "<div".Helpers::getContainer('html')->attributes($attributes) . ">";
+  public function withContents($contents, $opened = 0) {
+    $this->contents = $contents;
+    $this->opened = $opened;
+    return $this;
+  }
 
+  public function __toString() {
+    $name = $this->name;
+    $attributes = Helpers::add_class($this->attributes, 'panel-group');
+    $attributes = Helpers::add_class($attributes, $name, 'id');
+
+    $string = "<div".Helpers::getContainer('html')->attributes($attributes) . ">";
+    $count = 1;
+    foreach($this->contents as $content) {
+      $heading = $content[0];
+      $body = $content[1];
+      $panelAttributes = isset($content[2]) ? $content[2] : array();
+      $panelAttributes = Helpers::add_class($panelAttributes, 'panel panel-default');
+      
+      $bodyAttributes = array('class' => 'panel-collapse collapse panel-body',
+			      'id' => "$name-$count");		      
+      $bodyAttributes = $count == $this->opened ? Helpers::add_class($bodyAttributes, 'in') : $bodyAttributes;
+      
+      $string .= "<div" . Helpers::getContainer('html')->attributes($panelAttributes) .">";
+      $string .= "<div class='panel-heading'>";
+      $string .= "<h4 class='panel-title'>";
+      $string .= "<a class='accordion-toggle' data-toggle='collapse' data-parent='#$name' href='#$name-$count'>";
+      $string .= $heading;
+      $string .= "</a>";
+      $string .= "</h4>";
+      $string .= "</div>";
+      
+
+      $string .= "<div" .  Helpers::getContainer('html')->attributes($bodyAttributes) . ">";      
+      $string .= $body;
+      $string .= "</div>";
+      $string .= "</div>";
+      $count += 1;
+    }
     $string .= "</div>";
 
     return $string;
--- tests/AccordionTest.php
+++ tests/AccordionTest.php
@@ -2,37 +2,96 @@
 
 include_once '_start.php';
 
-/* <div class='panel-group' id='accordion'
-/* <div class="panel panel-default"> */
-/* <div class="panel-heading"> */
-/* <h4 class="panel-title"> */
-/* <a class="accordion-toggle" */
-/* data-toggle="collapse" */
-/* data-parent="#accordion" */
-/* href="#collapse{{ $achievement->id }}"> */
-/* {{ $achievement->title }}</a> *//*  */
-/* </h4> */
-/* </div> */
-/* <div id="collapse{{ $achievement->id }}" */
-/* class="panel-collapse collapse panel-body"> */
-/* <p> */
-/* {{ $achievement->description }} */
-/* </p> */
-/* <p> */
-/* Achieved by {{ $achievement->users->count() }} member(s). */
-/* <p> */
-/* <a href='{{ route('achievements.show', $achievement->id) }}'>See more information about this achievement</a> */
-/* </p> */
-/* </div> */
-/* </div> */
 
 use Bootstrapper\Accordion;
 
 class AccordionTest extends BootstrapperWrapper
 {
 
-  public function testWeCanCreateAnAccordion() {
+  public function getMatcher() {
+    return array(
+      'tag' => 'div',
+      'attributes' => array(
+        'class' => 'panel-group',
+        'id' => 'test'
+      ),
+      'child' => array(
+        'tag' => 'div',
+        'attributes' => array(
+          'class' => 'panel panel-default'
+        ),
+        'child' => array(
+          'tag' => 'div',
+          'attributes' => array(
+            'class' => 'panel-heading'
+          ),
+          'child' => array(
+            'tag' => 'h4',
+            'attributes' => array(
+              'class' => 'panel-title'
+            ),
+            'child' => array(
+              'tag' => 'a',
+              'attributes' => array(
+                'class' => 'accordion-toggle',
+                'data-toggle' => 'collapse',
+                'data-parent' => '#test',
+                'href' => '#test-1',
+              ),
+              'content' => 'First',
+            )
+          )
+        ),
+        'descendant' => array(
+          'tag' => 'div',
+          'attributes' => array(
+            'id' => 'test-1',
+            'class' => 'panel-collapse collapse panel-body'
+          ),
+          'content' => "Contents of first"
+        )
+      ),
+      'descendant' => array(
+        'tag' => 'div',
+        'attributes' => array(
+          'class' => 'panel panel-default'
+        ),
+        'child' => array(
+          'tag' => 'div',
+          'attributes' => array(
+            'class' => 'panel-heading'
+          ),
+          'child' => array(
+            'tag' => 'h4',
+            'attributes' => array(
+              'class' => 'panel-title'
+            ),
+            'child' => array(
+              'tag' => 'a',
+              'attributes' => array(
+                'class' => 'accordion-toggle',
+                'data-toggle' => 'collapse',
+                'data-parent' => '#test',
+                'href' => '#test-2',
+              ),
+              'content' => 'Second',
+            )
+          )
+        ),
+        'descendant' => array(
+          'tag' => 'div',
+          'attributes' => array(
+            'id' => 'test-2',
+            'class' => 'panel-collapse collapse panel-body'
+          ),
+          'content' => "Contents of second"
+        )
+      )
+    );
 
+  }
+
+  public function testWeCanCreateAnAccordion() {
     $accordion = Accordion::create('test');
 
     $matcher = array(
@@ -45,6 +104,64 @@ class AccordionTest extends BootstrapperWrapper
 
     $this->assertHtml($matcher, $accordion);
 
+  }
+
+  public function testWeCanCreateAnAccordionWithAttributes() {
+
+    $accordion = Accordion::create('test', $this->testAttributes);
+
+    $matcher = array(
+      'tag' => 'div',
+      'attributes' => array(
+        'class' => 'foo panel-group',
+        'data-foo' => 'bar'
+      ),
+    );
+
+    $this->assertHtml($matcher, $accordion);
+  }
+
+  public function testWeCanCreateAccordionsWithContents() {
+
+    $accordion = Accordion::create('test')
+         ->withContents(array(
+      array('First', 'Contents of first'),
+      array('Second', 'Contents of second'),
+    ));
+
+    $matcher = $this->getMatcher();
+
+    $this->assertHtml($matcher, $accordion);
+  }
+
+  public function testWeCanOpenAnAccordionPanelByDefault() {
+    $accordion = Accordion::create('test')
+         ->withContents(array(array('First', 'Contents of first'),
+			      array('Second', 'Contents of second')),
+			1);
+
+    $matcher = $this->getMatcher();
+    $matcher['child']['descendant']['attributes']['class'] .= ' in';
+
+    $this->assertHtml($matcher, $accordion);
+  }
+
+  public function testWeCanAddAttributesToThePanels() {
+    $accordion = Accordion::create('test')
+         ->withContents(array(array('First',
+				    'Contents of first',
+				    $this->testAttributes),
+			      array('Second',
+				    'Contents of second')));
+
+    $matcher = $this->getMatcher();
+
+    $matcher['child']['attributes'] = array(
+      'class' => 'panel panel-default foo',
+      'data-foo' => 'bar'
+    );
+
+    $this->assertHtml($matcher, $accordion);
   }
 
 }
