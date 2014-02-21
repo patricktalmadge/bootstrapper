patricktalmadge/bootstrapper

commit 2b09f0613da4592bcba9feaf78bed223f30a9d75

Fix for #184

src/Bootstrapper/Button.php
@@ -138,7 +138,7 @@ class Button
     $attributes['href'] = Helpers::getContainer('url')->to($url);
 
     if (!Helpers::has_class($attributes, $types)) {
-      $attributes = Helpers::add_class($attributes, 'btn-link');
+      $attributes = Helpers::add_class($attributes, 'btn-default');
     }
     return static::storeButton('link', $value, $attributes, $hasDropdown);
   }

tests/ButtonTest.php
@@ -63,7 +63,7 @@ class ButtonTest extends BootstrapperWrapper
 
   public function testDeemphasizeLink()
   {
-    $button = Button::Link('#', 'foo', $this->testAttributes)->deemphasize();
+    $button = Button::link('#', 'foo', $this->testAttributes)->deemphasize();
     $matcher = $this->createMatcher('link');
     $matcher['tag'] = 'a';
     $matcher['attributes']['href'] = '#';
@@ -121,7 +121,7 @@ class ButtonTest extends BootstrapperWrapper
    */
   public function testLink($class)
   {
-    if($class == 'normal') $class = 'link';
+    if($class == 'normal') $class = 'default';
     $method = $class.'_link';
 
     $button = Button::$method('#', 'foo', $this->testAttributes)->__toString();

commit e0fca56eab9ce8cb9f0b7e43962f9b6882075cec

Deemphasize's button shouldn't have 'btn-default' CSS class.
Regression introduces in commit 2b09f06 and referenced in issue #184

src/Bootstrapper/Button.php
@@ -207,6 +207,8 @@ class Button
   {
     // Add class to attributes array
     $this->currentButton['attributes'] = Helpers::add_class($this->currentButton['attributes'], 'btn-link');
+    // Remove class 'btn-default' to attributes arrray    
+    $this->currentButton['attributes'] = Helpers::remove_class($this->currentButton['attributes'], 'btn-default');
 
     return $this;
   }