5f97c0035b9fa5508abb73e7fa5d1f7abd4fed7c
Patrick Rose <pjr0911025@googlemail.com>
2014-06-15 00:44:43 +0100

    Add breadcrumbs

---
+++ src/Bootstrapper/Breadcrumb.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Bootstrapper;
+
+class Breadcrumb
+{
+
+    private $links = [];
+
+    public function render()
+    {
+        $string = "<ol class='breadcrumb'>";
+        foreach($this->links as $text => $link) {
+            $string .= $this->renderLink($text, $link);
+        }
+        $string .= "</ol>";
+        return $string;
+    }
+
+    public function withLinks($links)
+    {
+
+        $this->links = $links;
+
+        return $this;
+    }
+
+    private function renderLink($text, $link)
+    {
+        $string = "";
+        if (is_string($text)) {
+            $string .= "<li>";
+            $string .= "<a href='{$link}'>{$text}</a>";
+        } else {
+            $string .= "<li class='active'>";
+            $string .= $link;
+        }
+        $string .= "</li>";
+        return $string;
+    }
+}
