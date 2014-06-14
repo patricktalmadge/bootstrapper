b066fb0ef9832baf0714deff40361d923ba4fda7
Patrick Rose <pjr0911025@googlemail.com>
2014-06-14 23:49:51 +0100

    Add alerts

---
+++ src/Bootstrapper/Alert.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Bootstrapper;
+
+class Alert
+{
+
+    const INFO = 'alert-info';
+    const SUCCESS = 'alert-success';
+    const WARNING = 'alert-warning';
+    const DANGER = 'alert-danger';
+
+    private $type;
+    private $contents;
+    private $isBlock = false;
+    private $isCloseable = false;
+
+    private function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function render()
+    {
+        $attributes = new Attributes(['class' => "alert {$this->type}"]);
+
+        if ($this->isBlock) {
+            $attributes['class'] = trim($attributes['class']) . ' alert-block';
+        }
+        if ($this->isCloseable) {
+            $attributes['class'] = trim($attributes['class']) . ' alert-dismissable';
+            $this->contents = "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>{$this->contents}";
+        }
+
+        return "<div {$attributes}>{$this->contents}</div>";
+    }
+
+    public function info()
+    {
+        return $this->setType(Alert::INFO);
+    }
+
+    public function success()
+    {
+        return $this->setType(Alert::SUCCESS);
+    }
+
+    public function warning()
+    {
+        return $this->setType(Alert::WARNING);
+    }
+
+    public function danger()
+    {
+        return $this->setType(Alert::DANGER);
+    }
+
+    public function withContents($contents)
+    {
+        $this->contents = $contents;
+
+        return $this;
+    }
+
+    public function block()
+    {
+        $this->isBlock = true;
+
+        return $this;
+    }
+
+    public function close()
+    {
+        $this->isCloseable = true;
+
+        return $this;
+    }
+}
---
+++ tests/spec/Bootstrapper/AlertSpec.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace spec\Bootstrapper;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class AlertSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Bootstrapper\Alert');
+    }
+
+    function it_can_be_rendered()
+    {
+        $this->render()->shouldBe("<div class='alert'></div>");
+    }
+
+    function it_can_be_given_a_type()
+    {
+        $types = ['info', 'success', 'warning', 'danger'];
+        foreach($types as $type) {
+            $this->$type()->render()->shouldBe("<div class='alert alert-{$type}'></div>");
+        }
+    }
+
+    function it_can_be_given_contents()
+    {
+        $this->withContents("Test")->render()->shouldBe("<div class='alert'>Test</div>");
+    }
+
+    function it_can_be_made_a_block_alert()
+    {
+        $this->block()->render()->shouldBe("<div class='alert alert-block'></div>");
+    }
+
+    function it_can_become_closeable()
+    {
+        $this->close()->render()->shouldBe("<div class='alert alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button></div>");
+    }
+}
+?>
