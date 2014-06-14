c594364aae1b85eb2c4617d1aae8faed40f3706d
Patrick Rose <pjr0911025@googlemail.com>
2014-06-15 00:12:19 +0100

    Add badges

---
+++ src/Bootstrapper/Badge.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bootstrapper;
+
+class Badge
+{
+
+    private $contents;
+
+    /**
+     * @var
+     */
+    private $attributes;
+
+    public function render()
+    {
+        $this->attributes['class'] = "badge {$this->attributes['class']}";
+
+        $attributes = new Attributes($this->attributes);
+
+        $string = "<span {$attributes}>{$this->contents}</span>";
+
+        return $string;
+    }
+
+    public function withContents($contents)
+    {
+        $this->contents = $contents;
+
+        return $this;
+    }
+
+
+    public function withAttributes($attributes)
+    {
+        if (!array_key_exists('class', $attributes)) {
+            $attributes['class'] = "";
+        }
+
+        $this->attributes = $attributes;
+
+        return $this;
+    }
+}
