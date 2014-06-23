f6706c03de79cdf3a4c89e8034741c7f17340ed2
Patrick Rose <pjr0911025@googlemail.com>
2014-06-23 16:06:30 +0100

    Add Form::horizontalModel and Form::inlineModel

--- src/Bootstrapper/Form.php
+++ src/Bootstrapper/Form.php
@@ -28,31 +28,34 @@ class Form extends FormBuilder
         return $this->open($attributes);
     }
 
-    public function validation($type, $label, $input, $attributes = []) {
+    public function validation($type, $label, $input, $attributes = [])
+    {
         $attributes['class'] = isset($attributes['class']) ? "form-group {$type} " . $attributes['class'] : "form-group {$type} ";
         $attributes = new Attributes($attributes);
+
         return "<div {$attributes}>{$label}{$input}</div>";
     }
 
     public function success($label, $input, $attributes = [])
     {
-        return($this->validation(Form::FORM_SUCCESS, $label, $input, $attributes));
+        return ($this->validation(Form::FORM_SUCCESS, $label, $input, $attributes));
     }
 
     public function warning($label, $input, $attributes = [])
     {
-        return($this->validation(Form::FORM_WARNING, $label, $input, $attributes));
+        return ($this->validation(Form::FORM_WARNING, $label, $input, $attributes));
     }
 
     public function error($label, $input, $attributes = [])
     {
-        return($this->validation(Form::FORM_ERROR, $label, $input, $attributes));
+        return ($this->validation(Form::FORM_ERROR, $label, $input, $attributes));
     }
 
     public function feedback($label, $input, $icon, $attributes = [])
     {
         $attributes['class'] = isset($attributes['class']) ? 'form-group has-feedback ' . $attributes['class'] : 'form-group has-feedback';
         $attributes = new Attributes($attributes);
+
         return "<div {$attributes}>{$label}{$input}<span class='glyphicon glyphicon-{$icon} form-control-feedback'></span></div>";
     }
 
@@ -60,6 +63,21 @@ class Form extends FormBuilder
     {
         $attributes['class'] = isset($attributes['class']) ? 'help-block ' . $attributes['class'] : 'help-block';
         $attributes = new Attributes($attributes);
+
         return "<span {$attributes}>{$helpText}</span>";
+    }
+
+    public function horizontalModel($model, $attributes = [])
+    {
+        $attributes['class'] = isset($attributes['class']) ? self::FORM_HORIZONTAL . ' ' . $attributes['class'] : self::FORM_HORIZONTAL;
+
+        return $this->model($model, $attributes);
+    }
+
+    public function inlineModel($model, $attributes = [])
+    {
+        $attributes['class'] = isset($attributes['class']) ? self::FORM_INLINE . ' ' . $attributes['class'] : self::FORM_INLINE;
+
+        return $this->model($model, $attributes);
     }
 }
--- tests/spec/Bootstrapper/FormSpec.php
+++ tests/spec/Bootstrapper/FormSpec.php
@@ -38,41 +38,63 @@ class FormSpec extends ObjectBehavior
      */
     function it_can_open_a_form()
     {
-        $this->open()->shouldBe('<form method="POST" action="foo" accept-charset="UTF-8"><input name="_token" type="hidden" value="foo">');
+        $this->open()->shouldBe(
+            '<form method="POST" action="foo" accept-charset="UTF-8"><input name="_token" type="hidden" value="foo">'
+        );
     }
 
     function it_can_open_an_inline_form()
     {
-        $this->inline()->shouldBe('<form method="POST" action="foo" accept-charset="UTF-8" class="form-inline"><input name="_token" type="hidden" value="foo">');
+        $this->inline()->shouldBe(
+            '<form method="POST" action="foo" accept-charset="UTF-8" class="form-inline"><input name="_token" type="hidden" value="foo">'
+        );
 
-        $this->inline(['class' => 'option'])->shouldBe('<form method="POST" action="foo" accept-charset="UTF-8" class="form-inline option"><input name="_token" type="hidden" value="foo">');
+        $this->inline(['class' => 'option'])->shouldBe(
+            '<form method="POST" action="foo" accept-charset="UTF-8" class="form-inline option"><input name="_token" type="hidden" value="foo">'
+        );
     }
 
     function it_can_open_a_horizontal_form()
     {
-        $this->horizontal()->shouldBe('<form method="POST" action="foo" accept-charset="UTF-8" class="form-horizontal"><input name="_token" type="hidden" value="foo">');
+        $this->horizontal()->shouldBe(
+            '<form method="POST" action="foo" accept-charset="UTF-8" class="form-horizontal"><input name="_token" type="hidden" value="foo">'
+        );
 
-        $this->horizontal(['class' => 'option'])->shouldBe('<form method="POST" action="foo" accept-charset="UTF-8" class="form-horizontal option"><input name="_token" type="hidden" value="foo">');
+        $this->horizontal(['class' => 'option'])->shouldBe(
+            '<form method="POST" action="foo" accept-charset="UTF-8" class="form-horizontal option"><input name="_token" type="hidden" value="foo">'
+        );
     }
 
     function it_can_show_validation()
     {
         $validations = ['success', 'warning', 'error'];
 
-        foreach($validations as $validation) {
-            $this->$validation('<div>label</div>', '<div>input</div>')->shouldBe("<div class='form-group has-{$validation}'><div>label</div><div>input</div></div>");
-            $this->$validation('<div>label</div>', '<div>input</div>', ['class' => 'foo'])->shouldBe("<div class='form-group has-{$validation} foo'><div>label</div><div>input</div></div>");
-            $this->$validation('<div>label</div>', '<div>input</div>', ['foo' => 'bar'])->shouldBe("<div foo='bar' class='form-group has-{$validation}'><div>label</div><div>input</div></div>");
+        foreach ($validations as $validation) {
+            $this->$validation('<div>label</div>', '<div>input</div>')->shouldBe(
+                "<div class='form-group has-{$validation}'><div>label</div><div>input</div></div>"
+            );
+            $this->$validation('<div>label</div>', '<div>input</div>', ['class' => 'foo'])->shouldBe(
+                "<div class='form-group has-{$validation} foo'><div>label</div><div>input</div></div>"
+            );
+            $this->$validation('<div>label</div>', '<div>input</div>', ['foo' => 'bar'])->shouldBe(
+                "<div foo='bar' class='form-group has-{$validation}'><div>label</div><div>input</div></div>"
+            );
         }
     }
 
     function it_can_show_feedback()
     {
-        $this->feedback('<div>label</div>', '<div>input</div>', 'foo')->shouldBe("<div class='form-group has-feedback'><div>label</div><div>input</div><span class='glyphicon glyphicon-foo form-control-feedback'></span></div>");
+        $this->feedback('<div>label</div>', '<div>input</div>', 'foo')->shouldBe(
+            "<div class='form-group has-feedback'><div>label</div><div>input</div><span class='glyphicon glyphicon-foo form-control-feedback'></span></div>"
+        );
 
-        $this->feedback('<div>label</div>', '<div>input</div>', 'foo', ['class' => 'foo'])->shouldBe("<div class='form-group has-feedback foo'><div>label</div><div>input</div><span class='glyphicon glyphicon-foo form-control-feedback'></span></div>");
+        $this->feedback('<div>label</div>', '<div>input</div>', 'foo', ['class' => 'foo'])->shouldBe(
+            "<div class='form-group has-feedback foo'><div>label</div><div>input</div><span class='glyphicon glyphicon-foo form-control-feedback'></span></div>"
+        );
 
-        $this->feedback('<div>label</div>', '<div>input</div>', 'foo', ['bar' => 'foo'])->shouldBe("<div bar='foo' class='form-group has-feedback'><div>label</div><div>input</div><span class='glyphicon glyphicon-foo form-control-feedback'></span></div>");
+        $this->feedback('<div>label</div>', '<div>input</div>', 'foo', ['bar' => 'foo'])->shouldBe(
+            "<div bar='foo' class='form-group has-feedback'><div>label</div><div>input</div><span class='glyphicon glyphicon-foo form-control-feedback'></span></div>"
+        );
     }
 
     function it_can_show_help_blocks()
@@ -81,4 +103,17 @@ class FormSpec extends ObjectBehavior
         $this->help('foo', ['class' => 'foo'])->shouldBe("<span class='help-block foo'>foo</span>");
         $this->help('foo', ['data-class' => 'foo'])->shouldBe("<span data-class='foo' class='help-block'>foo</span>");
     }
+
+    function it_can_be_given_a_model()
+    {
+        $types = [
+            'horizontalModel' => '<form method="POST" action="foo" accept-charset="UTF-8" class="form-horizontal"><input name="_token" type="hidden" value="foo">',
+            'inlineModel' => '<form method="POST" action="foo" accept-charset="UTF-8" class="form-inline"><input name="_token" type="hidden" value="foo">'
+        ];
+        foreach ($types as $type => $expected) {
+            $this->$type(new Foo())->shouldBe($expected);
+        }
+    }
 }
+
+class Foo{}
