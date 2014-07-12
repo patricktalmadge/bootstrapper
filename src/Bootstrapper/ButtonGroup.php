ea74428b0f885e93e1885866aaa159c3db355136
Patrick Rose <pjr0911025@googlemail.com>
2014-07-12 10:34:04 +0100

    Add default button group constant

--- src/Bootstrapper/ButtonGroup.php
+++ src/Bootstrapper/ButtonGroup.php
@@ -11,6 +11,8 @@ class ButtonGroup extends RenderedObject
     const LARGE = 'btn-group-lg';
     const SMALL = 'btn-group-sm';
     const EXTRA_SMALL = 'btn-group-xs';
+
+    const NORMAL = 'btn-default';
     const PRIMARY = 'btn-primary';
     const SUCCESS = 'btn-success';
     const INFO = 'btn-info';
