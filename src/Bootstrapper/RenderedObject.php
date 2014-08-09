6e04a3654df79f77e11b373a5cbddf251a204a94
Patrick Rose <pjr0911025@googlemail.com>
2014-08-09 21:08:50 +0100

    Add render as an abstract method

--- src/Bootstrapper/RenderedObject.php
+++ src/Bootstrapper/RenderedObject.php
@@ -1,13 +1,16 @@
 <?php
 namespace Bootstrapper;
 
+use Illuminate\Support\Facades\Facade;
 
-abstract class RenderedObject
+abstract class RenderedObject extends Facade
 {
 
     public function __toString()
     {
         return $this->render();
     }
+
+    public abstract function render();
 
 } 
