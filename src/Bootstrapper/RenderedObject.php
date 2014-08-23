0cbdcb025dc9f7aef24f156fc913c29d3905ee09
Patrick Rose <pjr0911025@googlemail.com>
2014-08-23 20:54:41 +0100

    Fail gracefully inside a __toString()

--- src/Bootstrapper/RenderedObject.php
+++ src/Bootstrapper/RenderedObject.php
@@ -8,7 +8,15 @@ abstract class RenderedObject extends Facade
 
     public function __toString()
     {
-        return $this->render();
+        try
+        {
+            return $this->render();
+        }
+        catch (\Exception $e)
+        {
+            $class = get_class($e);
+            return "<p class='bg-warning'>An exception of type <code>{$class}</code> was thrown with the message: <code>{$e->getMessage()}</code>";
+        }
     }
 
     public abstract function render();
