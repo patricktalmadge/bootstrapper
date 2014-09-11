c77934bcb236009febaa95a39c3a653759be4f1d
Gabriel Machado <gabriel.ms1@hotmail.com>
2014-09-10 23:22:40 -0300

    Tabs to Space

--- tests/spec/Bootstrapper/ButtonSpec.php
+++ tests/spec/Bootstrapper/ButtonSpec.php
@@ -138,11 +138,11 @@ class ButtonSpec extends ObjectBehavior
 
     function it_has_get_methods()
     {
-        $this->danger('Foo')->withAttributes(['Foo' => 'Bar']);
+        $this->danger('Foo')->withAttributes(['data-foo' => 'bar']);
 
         $this->getType()->shouldBe('btn-danger');
         $this->getValue()->shouldBe('Foo');
-		$this->getAttributes()->shouldBe(['Foo' => 'Bar']);
+        $this->getAttributes()->shouldBe(['data-foo' => 'bar']);
     }
 
 }
