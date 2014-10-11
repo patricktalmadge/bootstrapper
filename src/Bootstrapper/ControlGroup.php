710ffe61cf8752baff81d9b61793c9ce572a190c
Patrick Rose <pjr0911025@googlemail.com>
2014-10-11 11:08:59 +0100

    Refactor methods to be cleaner, add docblocks

--- src/Bootstrapper/ControlGroup.php
+++ src/Bootstrapper/ControlGroup.php
@@ -4,53 +4,86 @@ namespace Bootstrapper;
 
 use Bootstrapper\Exceptions\ControlGroupException;
 
+/**
+ * Creates Bootstrap 3 compliant control groups (for forms)
+ * @package Bootstrapper
+ */
 class ControlGroup extends RenderedObject
 {
 
-    protected $attributes = [];
-    protected $contents = [];
-    protected $controlSize;
-    protected $label;
-    protected $labelSize;
-    protected $help;
     /**
-     * @var Form
+     * @var array The attributes for control group
+     */
+    protected $attributes = [];
+
+    /**
+     * @var array The contents of the control groups
+     */
+    protected $contents = [];
+
+    /**
+     * @var string The size of the control group
+     */
+    protected $controlSize;
+
+    /**
+     * @var string The label of control group
+     */
+    protected $label;
+
+    /**
+     * @var string The size of the label
+     */
+    protected $labelSize;
+
+    /**
+     * @var string The help text for the input
+     */
+    protected $help;
+
+    /**
+     * @var Form Laravel's form builder
      */
     protected $formBuilder;
 
+    /**
+     * @param Form $formBuilder An instance of the Bootstrapper formbuiler
+     */
     public function __construct(Form $formBuilder)
     {
         $this->formBuilder = $formBuilder;
     }
 
+    /**
+     * Renders the control group
+     *
+     * @return string
+     */
     public function render()
     {
-        $attributes = new Attributes($this->attributes, ['class' => 'form-group']);
+        $attributes = new Attributes(
+            $this->attributes,
+            ['class' => 'form-group']
+        );
         $string = "<div {$attributes}>";
 
-        if ($this->label)
-        {
+        if ($this->label) {
             $string .= $this->renderLabel();
         }
 
-        if ($this->controlSize)
-        {
+        if ($this->controlSize) {
             $string .= $this->createControlDiv();
         }
 
-        if (is_array($this->contents))
-        {
+        if (is_array($this->contents)) {
             $string .= $this->renderArrayContents();
-        }
-        else
-        {
-            $string .=  $this->contents;
+        } else {
+            $string .= $this->contents;
         }
 
         $string .= $this->help;
 
-        if ($this->controlSize)
-        {
+        if ($this->controlSize) {
             $string .= "</div>";
         }
 
@@ -59,18 +92,34 @@ class ControlGroup extends RenderedObject
         return $string;
     }
 
-    public function withAttributes($attributes)
+    /**
+     * Set the attributes of the control group
+     *
+     * @param $attributes array The attributes array
+     * @return $this
+     */
+    public function withAttributes(array $attributes)
     {
         $this->attributes = $attributes;
 
         return $this;
     }
 
+    /**
+     * Adds the contents to the control group
+     *
+     * @param $contents string The contents of the control group
+     * @param $controlSize null|int The size of the form control
+     * @return $this
+     * @throws ControlGroupException If $controlSize is set and not between 1
+     * and 12
+     */
     public function withContents($contents, $controlSize = null)
     {
-        if ($controlSize && ($controlSize < 1 || $controlSize > 12))
-        {
-            throw new ControlGroupException('That content size is incorrect - it must be between 1 and 12');
+        if (isset($controlSize) && $this->sizeIsInvalid($controlSize)) {
+            throw new ControlGroupException(
+                'That content size is incorrect - it must be between 1 and 12'
+            );
         }
 
         $this->contents = $contents;
@@ -79,11 +128,21 @@ class ControlGroup extends RenderedObject
         return $this;
     }
 
+    /**
+     * Sets the label of the control group
+     *
+     * @param $label string The label
+     * @param $labelSize null|int The size of the label
+     * @return $this
+     * @throws ControlGroupException If $labelSize is set and not between 1
+     * and 12
+     */
     public function withLabel($label, $labelSize = null)
     {
-        if (isset($labelSize) && ($labelSize < 1 || $labelSize > 11))
-        {
-            throw new ControlGroupException('That label size is incorrect - it must be between 1 and 12');
+        if (isset($labelSize) && $this->sizeIsInvalid($labelSize)) {
+            throw new ControlGroupException(
+                'That label size is incorrect - it must be between 1 and 12'
+            );
         }
 
         $this->label = $label;
@@ -92,6 +151,12 @@ class ControlGroup extends RenderedObject
         return $this;
     }
 
+    /**
+     * Adds a help block
+     *
+     * @param $help string The help information
+     * @return $this
+     */
     public function withHelp($help)
     {
         $this->help = $help;
@@ -99,29 +164,60 @@ class ControlGroup extends RenderedObject
         return $this;
     }
 
-    public function generate($label, $control, $help = null, $labelSize = null, $controlSize = null)
-    {
-        if (($labelSize && $controlSize) && ($labelSize + $controlSize < 1 || $labelSize + $controlSize > 12))
-        {
-            throw new ControlGroupException('That label size + control size must be between 1 and 12');
+    /**
+     * Generates a full control group with a label, control and help block
+     *
+     * @param $label string The label
+     * @param $control string The form control
+     * @param $help string The help text
+     * @param $labelSize int The size of the label
+     * @param $controlSize int The size of the form control
+     * @return $this
+     * @throws ControlGroupException if the sizes are invalid
+     */
+    public function generate(
+        $label,
+        $control,
+        $help = null,
+        $labelSize = null,
+        $controlSize = null
+    ) {
+        if ($this->sizesAreInvalid($labelSize, $controlSize)) {
+            throw new ControlGroupException(
+                'The label size + control size must be between 1 and 12'
+            );
         }
 
-        return $this->withLabel($label, $labelSize)->withContents($control, $controlSize)->withHelp($help);
+        return $this->withLabel($label, $labelSize)
+            ->withContents($control, $controlSize)
+            ->withHelp($help);
     }
 
+    /**
+     * Renders the contents if given as an array
+     *
+     * @return string
+     */
     protected function renderArrayContents()
     {
         $string = '';
-        foreach($this->contents as $item)
-        {
-            if(isset($item['label']))
-            {
-                $string .= call_user_func_array([$this->formBuilder, 'label'], $item['label']) . ' ';
+        foreach ($this->contents as $item) {
+            if (isset($item['label'])) {
+                $string .= call_user_func_array(
+                        [$this->formBuilder, 'label'],
+                        $item['label']
+                    ) . ' ';
             }
+
             $input_args = $item['input'];
             $type = $input_args['type'];
             unset($input_args['type']);
-            $string .= call_user_func_array([$this->formBuilder, $type], $input_args);
+
+            $string .= call_user_func_array(
+                [$this->formBuilder, $type],
+                $input_args
+            );
+
             $string .= '<br />';
         }
 
@@ -129,6 +225,8 @@ class ControlGroup extends RenderedObject
     }
 
     /**
+     * Renders the label
+     *
      * @return string
      */
     public function renderLabel()
@@ -150,10 +248,58 @@ class ControlGroup extends RenderedObject
     }
 
     /**
+     * Creates the div to surround the form control
+     *
      * @return string
      */
     public function createControlDiv()
     {
         return sprintf("<div class='col-sm-%s'>", $this->controlSize);
+    }
+
+    /**
+     * Checks if both the label size and control size are invalid
+     *
+     * @param $labelSize int The size of the label
+     * @param $controlSize int The size of the control group
+     * @return bool
+     */
+    protected function sizesAreInvalid($labelSize = null, $controlSize = null)
+    {
+        // If both are null then we have a valid size
+        if (!isset($labelSize) && !isset($controlSize)) {
+            return false;
+        }
+
+        // So at least one of these is null
+        if (isset($labelSize)) {
+            if ($this->sizeIsInvalid($labelSize)) {
+                return true;
+            }
+        } else {
+            $labelSize = 0;
+        }
+
+        if (isset($controlSize)) {
+            if ($this->sizeIsInvalid($controlSize)) {
+                return true;
+            }
+        } else {
+            $controlSize = 0;
+        }
+
+        return $this->sizeIsInvalid($labelSize + $controlSize);
+    }
+
+    /**
+     * Checks if the size is invalid
+     *
+     * @param $size int The size
+     * @return bool True if the size is below 1 or greater than 11,
+     * false otherwise
+     */
+    protected function sizeIsInvalid($size)
+    {
+        return $size < 1 || $size > 11;
     }
 }
