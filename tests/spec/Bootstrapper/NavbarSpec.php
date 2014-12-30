94185f19c65d20e85725f3dc2665aee7f59e664d
Patrick Rose <pjr0911025@googlemail.com>
2014-12-30 13:36:31 +0000

    Allow Inverse to be used as a Create

--- tests/spec/Bootstrapper/NavbarSpec.php
+++ tests/spec/Bootstrapper/NavbarSpec.php
@@ -102,4 +102,11 @@ class NavbarSpec extends ObjectBehavior
             "<div class='navbar navbar-default' role='navigation'><div class='container-fluid'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button></div><nav class='navbar-collapse collapse'></nav></div></div>"
         );
     }
+
+    function it_allows_you_to_use_inverse_as_a_shortcut()
+    {
+        $this->inverse('foo', ['data-foo' => 'bar'])->render()->shouldBe(
+            "<div class='navbar navbar-inverse foo' role='navigation' data-foo='bar'><div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button></div><nav class='navbar-collapse collapse'></nav></div></div>"
+        );
+    }
 }
