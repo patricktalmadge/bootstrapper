1aee6ef1df13fab5b2c79d4f4e1934367d46c97b
Patrick Rose <pjr0911025@googlemail.com>
2015-04-09 13:46:56 +0100

    Update default bootstrap. Closes #277

--- src/config/config.php
+++ src/config/config.php
@@ -4,7 +4,7 @@
  * Default config values
  */
 return [
-    'bootstrapVersion' => '3.3.0',
+    'bootstrapVersion' => '3.3.4',
     'jqueryVersion' => '2.1.0',
     'icon_prefix' => 'glyphicon'
 ];
